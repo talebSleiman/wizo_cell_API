bb40636c0b01eceee91aff3bbd385517242650c1
talebSleiman <talebsleiman4@gmail.com>
2023-05-13 22:14:06 +0300

    delete files

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ommxw^n(qw1c&@)fddl#5$td36#_3bdxmjm%e75vug=bqbffqp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -130,4 +130,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-django_heroku.settings(locals())
+django_heroku.settings()
