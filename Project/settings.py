7e20b33a14e35a113d96b28cbb404bfeb2ba514e
talebSleiman <talebsleiman4@gmail.com>
2023-06-05 22:32:11 +0300

    change

--- Project/settings.py
+++ Project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'corsheaders',
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
